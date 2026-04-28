0ca3fa76d850d3b8946178fbb78fd0ac9c9c62be
edinburgh <edinburgh598042239@gmail.com>
2026-04-28 22:49:51 +0800

    feat: 初始化Taro微信小程序项目 - 父母成长等级测试

---
+++ taro-parents-test/config/index.js
@@ -0,0 +1,38 @@
+const config = {
+  projectName: 'taro-parents-test',
+  date: '2026-4-22',
+  designWidth: 750,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
+  plugins: [],
+  defineConstants: {},
+  copy: {
+    patterns: [],
+    options: {}
+  },
+  framework: 'react',
+  compiler: 'webpack5',
+  cache: {
+    enable: false
+  },
+  mini: {
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {}
+      }
+    }
+  }
+};
+
+module.exports = function (merge) {
+  if (process.env.NODE_ENV === 'development') {
+    return merge({}, config, require('./dev'));
+  }
+  return merge({}, config, require('./prod'));
+};
